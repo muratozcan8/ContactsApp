55f7faa9afd43e3e1eefa596dfd6bd5596cb11a3
muratozcan8 <mrtzcn08@gmail.com>
2023-10-21 23:03:08 +0300

    Initial commit

---
+++ app/src/main/java/com/muratozcan/contactsapp/ui/fragment/PersonRegisterFragment.kt
@@ -0,0 +1,16 @@
+package com.muratozcan.contactsapp.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.muratozcan.contactsapp.R
+
+class PersonRegisterFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_person_register, container, false)
+    }
+
+}
